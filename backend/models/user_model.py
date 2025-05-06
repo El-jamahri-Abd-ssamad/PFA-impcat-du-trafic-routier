55e991e9f9779a0a8bc3f647d16abb492e923d39
salma-hadouchi <hadouchisama3@gmail.com>
2025-05-06 13:02:44 +0000

    m

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from dataclasses import dataclass
 
 
@@ -6,3 +7,11 @@ class User:
     email: str
     password : str
     
+=======
+""" class User:
+
+    def __init__(self, email: str, password: str) -> None:
+        self.email: str = email
+        self.password: str = password
+ """
+>>>>>>> dd5d395 (appProject)
