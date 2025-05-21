541dc6a67c5abf8e2524f7630f2bd521c2c1ff77
Gustavoamad <abdssamadeljamahri@gmail.com>
2025-05-21 17:49:48 +0100

    l'ajoute d'un modèle de configuration .env.example

---
+++ app/app.config.js
@@ -0,0 +1,34 @@
+import 'dotenv/config'; // pour lire .env
+
+export default {
+  expo: {
+    name: "AppProject",
+    slug: "AppProject",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      apiHost: process.env.API_HOST,
+      apiPort: process.env.API_PORT
+    }
+  }
+};
